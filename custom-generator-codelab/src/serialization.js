1a3d8438a543fc6938944583f7135e7606758a55
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-03-01 11:31:22 +0100

    Add empty template for Java static method and extract template workspace into separate file
    Signed-off-by: Valentin Herrmann <accounts@valentin-herrmann.com>

--- custom-generator-codelab/src/serialization.js
+++ custom-generator-codelab/src/serialization.js
@@ -7,6 +7,7 @@
 import * as Blockly from 'blockly/core';
 import LocalStorageManager from './utils/LocalStorageManager';
 import { IdeBridge } from './utils/IdeBridge';
+import emptyTemplate from './emptyTemplate.json';
 
 
 /**
@@ -35,7 +36,6 @@ export const load = function(workspace) {
     console.warn('No file selected, skipping workspace save.');
     return;
   }
-  const emptyTemplate = {"blocks":{"languageVersion":0,"blocks":[{"type":"java_static_method_noreturn","id":"0-~]xj!ER5LuqkjtzKSY","x":92,"y":164,"extraState":"<mutation xmlns=\"http://www.w3.org/1999/xhtml\"></mutation>","icons":{"comment":{"text":"Das Hauptprogramm (main-Methode): \nAlles, was ausgeführt werden soll, \nmuss in diese Methode eingefügt werden. \nObjekte erstellen, Methoden aufrufen, ...\n\nDas Hauptprogramm wird automatisch \ngestartet, wenn du auf 'Play' drückst.","pinned":true,"height":69.96484375,"width":305.83197021484375}},"fields":{"NAME":"main"}}]}};
   const data = LocalStorageManager.loadWorkspace(className) || JSON.stringify(emptyTemplate);
 
   // Don't emit events during loading.
